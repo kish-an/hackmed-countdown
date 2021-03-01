5f264e8c657a50ba439a7df79f66d461b85fda29
Kishan <kishan.o@hotmail.com>
2021-03-01 00:59:26 +0000

    Update carousel

--- index.js
+++ index.js
@@ -77,5 +77,5 @@ const carousel = document.querySelector('.main-carousel');
 const flkty = new Flickity(carousel, {
     contain: true,
     wrapAround: true,
-    autoPlay: true,
+    autoPlay: 8000,
 });
